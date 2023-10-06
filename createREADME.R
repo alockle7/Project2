95d060e3280ea82a252e04789c04c6f8669a32b0
Autumn Locklear <alockle7@ncsu.edu>
2023-10-06 15:43:18 -0400

    First commit

---
+++ createREADME.R
@@ -0,0 +1,8 @@
+#author: Autumn Locklear
+#purpose: Render censusVingette.Rmd as a .md file called README.md for my repo.
+
+rmarkdown::render(
+  input = "censusVignette.Rmd",
+  output_format = "github_document",
+  output_file = "README.md"
+)
